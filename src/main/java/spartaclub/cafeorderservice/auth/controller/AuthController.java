8c92c2dccd673e59e2e525b547774f2d000f3884
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-05-11 12:40:31 +0900

    feat: JWT기반 로그인 기능 구현

--- src/main/java/spartaclub/cafeorderservice/auth/controller/AuthController.java
+++ src/main/java/spartaclub/cafeorderservice/auth/controller/AuthController.java
@@ -1,4 +1,30 @@
 package spartaclub.cafeorderservice.auth.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spartaclub.cafeorderservice.auth.dto.LoginRequest;
+import spartaclub.cafeorderservice.auth.dto.LoginResponse;
+import spartaclub.cafeorderservice.auth.service.AuthService;
+import spartaclub.cafeorderservice.common.response.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class AuthController {
+
+    private final AuthService authService;
+
+    // 로그인 API
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login (
+            @RequestBody @Valid LoginRequest request) {
+        LoginResponse response = authService.login(request);
+
+        return ResponseEntity.ok(ApiResponse.of(response));
+    }
 }
--- src/main/java/spartaclub/cafeorderservice/auth/dto/LoginRequest.java
+++ src/main/java/spartaclub/cafeorderservice/auth/dto/LoginRequest.java
@@ -1,4 +1,19 @@
 package spartaclub.cafeorderservice.auth.dto;
 
-public record LoginRequest() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+
+        @NotBlank(message = "전화번호를 입력해주세요")
+        @Pattern(
+                regexp = "^010-\\d{4}-\\d{4}$",
+                message = "전화번호 형식이 올바르지 않습니다."
+        )
+        String phone,
+
+        @NotBlank(message = "PIN을 입력해주세요")
+        @Size(min = 4, max = 4, message = "PIN은 4자리여야 합니다")
+        String pin
+) {}
